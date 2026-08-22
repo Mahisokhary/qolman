7fdd8b50c2c2a35a67f4500ef961e12f6196a89d
Qoqnus master <mahisokhary@gmail.com>
2026-08-22 13:38:12 +0330

    qolman/level: Created typedefs and structs to define log level (GitHub #34)
    qolman/level:
    - Created public struct qolman_level
    - Created typedef qolman_level_t

--- qolman/level/level.h
+++ qolman/level/level.h
@@ -5,6 +5,19 @@
 extern "C" {
 #endif
 
+/*
+ * Public implementation of log level
+ */
+struct qolman_level {
+	int importance;
+	char *label;
+};
+
+/*
+ * Pointer to actual level
+ */
+typedef const struct qolman_level *qolman_level_t;
+
 #ifdef __cplusplus
 }
 #endif

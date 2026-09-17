bbf257bde8a6253fdad70a53bc5c5acd4317509e
Qoqnus master <mahisokhary@gmail.com>
2026-09-17 09:48:03 +0330

    qolman/manager: Added functions to handle records (GitHub #130)
    qolman/manager:
    - Added function qolman_manager_handler
    
    test/qolman/handler:
    - Updated function _handler_create to be public and available to other tests
    
    test/qolman/manager:
    - Used handler from test group handler which is designed to be testable
    - Updated test qolman_manager_create_destroy to test function qolman_manager_handle

--- test/qolman/manager.c
+++ test/qolman/manager.c
@@ -1,6 +1,11 @@
 #include <test/test.h>
 #include <qolman/qolman.internal.h>
 
+/*
+ * Handler defined in test group handler
+ */
+extern qolman_result_t _handler_create(qolman_handler_t *handler);
+
 bool test_qolman_manager_create_destroy() {
 	test_run_log("qolman_manager_create_destroy");
 	qolman_manager_t manager;
@@ -11,7 +16,7 @@ bool test_qolman_manager_create_destroy() {
 
 	bool success = 1;
 	success &= qolman_manager_create(&manager, &qolman_formatter_text) == QOLMAN_RESULT_OK;
-	success &= qolman_handler_stdout(&handler) == QOLMAN_RESULT_OK;
+	success &= _handler_create(&handler) == QOLMAN_RESULT_OK;
 
 	handlers[0] = handler;
 	success &= qolman_manager_handlers_set(manager, handlers, handlers_length) == QOLMAN_RESULT_OK;
@@ -26,6 +31,12 @@ bool test_qolman_manager_create_destroy() {
 	success &= qolman_manager_formatter(manager) == NULL;
 	success &= qolman_manager_formatter_set(manager, &qolman_formatter_text) == QOLMAN_RESULT_OK;
 	success &= qolman_manager_formatter(manager) == &qolman_formatter_text;
+
+	qolman_record_t record;
+	success &= qolman_record_create(&record, 1676385060, &level, "god", "phoenix", NULL) == QOLMAN_RESULT_OK;
+	success &= qolman_manager_handle(manager, record) == QOLMAN_RESULT_OK;
+	success &= strcmp("[2023-02-14 18:01:00] [godlevel] [god] phoenix\n", *(char **) handler->data) == 0;
+	success &= qolman_record_destroy(record) == QOLMAN_RESULT_OK;
 
 	success &= qolman_manager_destroy(manager) == QOLMAN_RESULT_OK;
 	test_result_log(success);
